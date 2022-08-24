06698cf92d5cebc8845618fc0f64b56fe3cb14bb
hossein khoshyari <hossein12khoshyari@gmail.com>
2022-08-25 00:28:30 +0430

    [feat] App : implement application routing in app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,7 +2,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'auth/login',
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.module').then((module) => {
+        return module.AuthModule;
+      }),
+  },
+  {
+    path: '**',
+    redirectTo: 'auth/login',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
