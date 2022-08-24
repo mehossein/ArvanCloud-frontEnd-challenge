ffd2eb772fb9293a95c9ec6ebef8e0ca91ca7019
hossein khoshyari <hossein12khoshyari@gmail.com>
2022-08-24 17:16:55 +0430

    [feat] Dependency : imports ToasterModule in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 // angular
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 // app
 import { AppComponent } from './app.component';
 import { AlertService } from '@shared/services';
 import { AppRoutingModule } from './app.routing';
 
+// 3'rd party
+import { ToastrModule } from 'ngx-toastr';
+
 @NgModule({
   bootstrap: [AppComponent],
   providers: [AlertService],
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    // angular
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    // 3'rd party
+    ToastrModule.forRoot(),
+  ],
 })
 export class AppModule {}
