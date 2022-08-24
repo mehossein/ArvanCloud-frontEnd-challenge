979e1d8da5372d915c19f083349508a2fd609d24
hossein khoshyari <hossein12khoshyari@gmail.com>
2022-08-24 14:11:48 +0430

    [feat] Dependency : provide alert.service in app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // app
 import { AppComponent } from './app.component';
+import { AlertService } from '@shared/services';
 import { AppRoutingModule } from './app.routing';
 
 @NgModule({
   bootstrap: [AppComponent],
+  providers: [AlertService],
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
 })
