a53a754aa5c4401c479915c2d80975a0ecd0bf55
hossein khoshyari <hossein12khoshyari@gmail.com>
2022-08-24 17:27:48 +0430

    [feat] core : implements core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,32 @@
+// angular
+import { CommonModule } from '@angular/common';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// app
+import { HttpsInterceptor } from './services';
+
+@NgModule({
+  imports: [
+    // angular
+    CommonModule,
+    HttpClientModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpsInterceptor,
+    },
+  ],
+})
+export class CoreModule {
+  constructor(
+    @Optional()
+    @SkipSelf()
+    parentModule: CoreModule
+  ) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import only in AppModule');
+    }
+  }
+}
