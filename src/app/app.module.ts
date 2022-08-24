147acaa6f1374e3f40d9f7f58906a6b012753c65
hossein khoshyari <hossein12khoshyari@gmail.com>
2022-08-24 17:32:54 +0430

    [feat] Core : import core module in app.module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 // app
 import { AppComponent } from './app.component';
 import { AlertService } from '@shared/services';
+import { CoreModule } from '@core/core.module';
 import { AppRoutingModule } from './app.routing';
 
 // 3'rd party
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  bootstrap: [AppComponent],
-  providers: [AlertService],
   declarations: [AppComponent],
   imports: [
     // angular
@@ -23,6 +22,11 @@ import { ToastrModule } from 'ngx-toastr';
 
     // 3'rd party
     ToastrModule.forRoot(),
+
+    //app
+    CoreModule,
   ],
+  bootstrap: [AppComponent],
+  providers: [AlertService],
 })
 export class AppModule {}
