01addf76501feca2fa336a4d4e0cd8a3c8161e13
hossein khoshyari <hossein12khoshyari@gmail.com>
2022-08-27 04:34:51 +0430

    [feat] Article  : create article form component with angualr cli

---
+++ src/app/article/components/article-form/article-form.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'arvan-article-form',
+  templateUrl: './article-form.component.html',
+  styleUrls: ['./article-form.component.scss'],
+})
+export class ArticleFormComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
